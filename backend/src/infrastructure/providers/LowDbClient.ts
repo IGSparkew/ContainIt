a882b424eeb2c594ba2dd2fa3077a84c7520f6bc
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2026-04-17 00:45:18 +0200

    repository done and begin presentation

---
+++ backend/src/infrastructure/providers/LowDbClient.ts
@@ -0,0 +1,32 @@
+import { LowSync } from "lowdb";
+import { ILowDbClient } from "../../application/providers/ILowDbClient.js";
+import { DbData } from "../../domain/models/DbData.js";
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from "fs";
+import { JSONFileSync } from "lowdb/node";
+
+const path = 'data/db.json';
+
+export class LowDbClient implements ILowDbClient {
+
+    constructor(private db: LowSync<DbData>) {
+        if (existsSync(path)) {
+            try {
+                JSON.parse(readFileSync(path, 'utf-8'))
+            } catch {
+                unlinkSync(path)
+            }
+        }
+    
+        if (!existsSync(path)) {
+            writeFileSync(path, '{ "instances": [], "volumes": [], "networks": [] }', { encoding: 'utf8' })
+        }
+        const adapter = new JSONFileSync<DbData>(path)
+        this.db = new LowSync(adapter, { instances: [], volumes: [], networks: [] })
+        this.db.read()
+    }
+
+    getDb(): LowSync<DbData> {
+        return this.db;
+    }
+
+}
---
+++ backend/src/presentation/http/helpers/impl/HttpResponse.ts
@@ -0,0 +1,12 @@
+import { IHttpResponse } from "../IHttpResponse.js";
+
+
+export class HttpResponse implements IHttpResponse {
+    statusCode: number;
+    body: Record<string, string>;
+
+    constructor(statusCode: number, body: any) {
+        this.statusCode = statusCode;
+        this.body = body;
+    }
+}
